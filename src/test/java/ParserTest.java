624575bbf00bc5ad5d3e775212c13fc609a30173
yuny0623 <overman522@gmail.com>
2022-11-23 14:49:45 +0900

    Test: add jsonparser test.

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,9 +1,24 @@
+import org.json.simple.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ParserTest {
 
     @Test
     public void json파서테스트(){
+        // given
+        JSONObject inner = new JSONObject();
+        inner.put("name","razlo");
+        inner.put("age", "99");
+        inner.put("address","seoul");
+        JSONObject outer = new JSONObject();
+        outer.put("info", inner);
 
+        // when
+        String json = outer.toJSONString();
+        String resultJson = "{\"info\":{\"address\":\"seoul\",\"name\":\"razlo\",\"age\":\"99\"}}";
+
+        // then
+        Assert.assertEquals(json, resultJson);
     }
 }
