60516eb741e53d9881eb94155919dff7e8b8004d
yuny0623 <overman522@gmail.com>
2022-11-23 15:16:00 +0900

    Feat: add switch to verify command.

--- src/main/java/org/client/Utils/JsonGenerator.java
+++ src/main/java/org/client/Utils/JsonGenerator.java
@@ -5,11 +5,46 @@ import org.json.simple.JSONObject;
 public class JsonGenerator {
     public static String generateJson(String action){
         JSONObject jsonObject = new JSONObject();
+        String[] inputs = action.split(" ");
+        String command = inputs[0];
+        String result = "";
 
-        switch(action){
-
+        switch(command){
+            case "move":
+                String x_val = inputs[1];
+                String y_val = inputs[2];
+                jsonObject.put("command", "move");
+                jsonObject.put("x", x_val);
+                jsonObject.put("y", y_val);
+                result = jsonObject.toJSONString();
+                break;
+            case "attack":
+                jsonObject.put("command", "attack");
+                result = jsonObject.toJSONString();
+                break;
+            case "monsters":
+                jsonObject.put("command", "monsters");
+                result = jsonObject.toJSONString();
+                break;
+            case "users":
+                jsonObject.put("command", "users");
+                result = jsonObject.toJSONString();
+                break;
+            case "chat":
+                String opponent = inputs[1];
+                String content = inputs[2];
+                jsonObject.put("command", "chat");
+                jsonObject.put("opponent", opponent);
+                jsonObject.put("content", content);
+                result = jsonObject.toJSONString();
+                break;
+            case "bot":
+                jsonObject.put("command","bot");
+                result = jsonObject.toJSONString();
+                break;
+            default:
+                result = null;
         }
-
-        return jsonObject.toJSONString();
+        return result;
     }
 }
